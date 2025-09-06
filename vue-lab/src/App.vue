2e6f1c8da2735a56fde6152fae9eabc269b19ade
kent04-23 <kentangel2004@gmail.com>
2025-09-06 11:40:29 +0800

    lab-vue sir

---
+++ vue-lab/src/App.vue
@@ -0,0 +1,29 @@
+<!-- <script setup lang="ts">
+import HelloWorld from './components/kent.vue'
+</script> -->
+
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Charles" language="Bisagalog" />
+  <StudentCard name="Simon" language="Python" />
+  <StudentCard name="Charlie" language="Java" />
+</template>
+
+<script setup>
+import StudentCard from './components/kent.vue'
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
