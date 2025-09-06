9a0b7914cd17fa3ae75684e7f552c07238410af3
GUIIX10 <tolentinorobin65@gmail.com>
2025-09-06 12:00:38 +0800

    elpelp

---
+++ vue-lab/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Alice" language="JavaScript" />
+  <StudentCard name="Bob" language="Python" />
+  <StudentCard name="Charlie" language="Java" />
+</template>
+
+<script setup>
+import StudentCard from './components/robin.vue'
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
