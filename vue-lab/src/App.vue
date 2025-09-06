2a6d5323a81a20dcd53499c8554bfc7f72f26bc0
JoshuaRazon10 <Joshuarazon731@gmail.com>
2025-09-06 11:26:45 +0800

    Add Vue lab with StudentCard component

---
+++ vue-lab/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import StudentCard from './components/fullname.vue'
+</script>
+
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Joshua" language="Java" />
+  <StudentCard name="Simon" language="Java" />
+  <StudentCard name="Kent" language="Javascript" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
