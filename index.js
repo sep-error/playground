164ab66d11f882eb705075ace02f7b4c3b63abe5
Ulrick Balajadia <balajadiaced@gmail.com>
2025-09-13 11:35:10 +0800

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { add, multiply } from './modules/mathUtils.js';
 import { getRandomLeader } from './modules/team.js';
 import { tasks } from './modules/tasks.js';
 import { grades } from './modules/grades.js';
-import { divide } from './modules/mathUtils.js';
+// import { divide } from './modules/mathUtils.js';
 
 students.forEach(s => {
   console.log(`${s.name} loves ${s.language}`);
@@ -11,7 +11,7 @@ students.forEach(s => {
 
 console.log("Add:", add(2, 3));           
 console.log("Multiply:", multiply(4, 2)); 
-console.log("Multiply:", divide(4, 2)); 
+// console.log("Multiply:", divide(4, 2)); 
 
 const leader = getRandomLeader();
 console.log(`Team leader is ${leader.name}, who loves ${leader.language}`);
--- modules/mathUtils.js
+++ modules/mathUtils.js
@@ -6,3 +6,6 @@ export function multiply(a, b) {
   return a * b;
 }
 
+// export function divide(a, b) {
+//   return a / c; 
+// }
