a0b207a4399f0613593ebd85c14a8e53aa36245b
Drewit-Stack <celisandrew09@gmail.com>
2025-09-13 11:10:45 +0800

    JS

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,10 @@
 import { add, multiply } from './modules/mathUtils.js';
+import { students } from './modules/students.js';
 
 console.log("Add:", add(2, 3));          // 5
 console.log("Multiply:", multiply(4, 2)); // 8
+
+
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
