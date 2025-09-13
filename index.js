d95ee3baf6a51335daa21a901b3843756391309b
kent04-23 <Kentangel2004@gmail.com>
2025-09-13 12:10:45 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { add, multiply } from './modules/mathUtils.js';
+import { students } from './modules/students.js';
+import { add, Division, multiply, Subtraction } from './modules/mathUtils.js';
+
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
 
 console.log("Add:", add(2, 3));          // 5
-console.log("Multiply:", multiply(4, 2)); // 8n
+console.log("Multiply:", multiply(4, 2)); // 8
+console.log("Division:", Division(4, 2)); // 8
+console.log("Subtraction:", Subtraction(8, 2)); // 8
+
--- modules/mathUtils.js
+++ modules/mathUtils.js
@@ -5,3 +5,10 @@ export function add(a, b) {
 export function multiply(a, b) {
   return a * b;
 }
+
+export function Division(a, b) {
+  return a / b;
+}
+export function Subtraction(a, b) {
+  return a - b;
+}
