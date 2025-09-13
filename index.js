e4c0412ab36d6c1c9def89690f69201981f3a22d
JoshuaRazon10 <joshuarazon731@gmail.com>
2025-09-13 11:27:47 +0800

    Pa accept po sir

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { add, multiply } from './modules/mathUtils.js';
 import { students } from './modules/students.js';
+import { getRandomLeader } from './modules/team.js';
 
 console.log("Add:", add(2, 3));          
 console.log("Multiply:", multiply(4, 2)); 
