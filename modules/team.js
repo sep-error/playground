6f1c3c63af8b4f4f1a88ac5062bd0010f0bf7d98
GUIIX10 <tolentinorobin65@gmail.com>
2025-09-13 12:41:00 +0800

    Complete Part 1: JS fundamentals, mathUtils and students modules

--- modules/team.js
+++ modules/team.js
@@ -1,6 +1,11 @@
-import { students } from './students.js';
+import { add, multiply } from './modules/mathUtils.js';
+import { students } from './modules/students.js';
 
-export function getRandomLeader() {
-  const index = Math.floor(Math.random() * students.length);
-  return students[index];
-}
+console.log("Add:", add(2, 3));          // 5
+console.log("Multiply:", multiply(4, 2)); // 8
+
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
+
+node index.js
