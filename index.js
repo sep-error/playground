acf6be566421933fc501297dba86144592d3e772
JoshuaRazon10 <joshuarazon731@gmail.com>
2025-09-13 11:19:38 +0800

    Pa accept po sir

---
+++ index.js
@@ -0,0 +1,10 @@
+import { add, multiply } from './modules/mathUtils.js';
+import { students } from './modules/students.js';
+
+console.log("Add:", add(2, 3));          
+console.log("Multiply:", multiply(4, 2)); 
+
+
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
