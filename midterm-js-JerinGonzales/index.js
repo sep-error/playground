0ade2b3519bdde6e5dd88e56f1b27eda9a2be8b0
jeringonzales29 <jerinnathalygonzales29>
2025-09-26 21:00:02 -0700

    jerin to oy

---
+++ midterm-js-JerinGonzales/index.js
@@ -0,0 +1,5 @@
+import { students } from './students.js';
+import { add, multiply } from './mathUtils.js';
+
+console.log("Add:", add(2, 3));        // 5
+console.log("Multiply:", multiply(4, 2)); // 8
