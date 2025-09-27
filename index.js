042b8d42ef369fc3ce420f89d324b4ed0295768b
GUIIX10 <tolentinorobin65@gmail.com>
2025-09-27 12:17:35 +0800

    helppp

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import { add, multiply } from './midterm-js-kentangelsanandres/mathUtils.js';
-import { students } from './midterm-js-kentangelsanandres/students.js';
+// import { add, multiply } from './midterm-js-kentangelsanandres/mathUtils.js';
+// import { students } from './midterm-js-kentangelsanandres/students.js';
+// console.log("Add:", add(2, 3));        // 5
+// console.log("Multiply:", multiply(4, 2)); // 8
+
+
+// students.forEach(s => {
+//   console.log(`${s.name} loves ${s.language}`);
+// });
+
+import { add, multiply } from './midterm-js-robin/mathUtils.js';
+import { students } from './midterm-js-robin/students.js';
 console.log("Add:", add(2, 3));        // 5
 console.log("Multiply:", multiply(4, 2)); // 8
-
-
 students.forEach(s => {
   console.log(`${s.name} loves ${s.language}`);
 });
