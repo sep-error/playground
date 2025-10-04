03ac30356676ca44d9fa594ef45202b9a1c3042b
Drewit-stack <celisandrew09@gmail.com>
2025-10-04 08:22:17 +0800

    Add midterm exam answer

---
+++ midterm-js-johnandrew/index.js
@@ -0,0 +1,11 @@
+//Sorry po sir super late, nag ka family prob po, salamat!!
+
+import { add, multiply } from './mathUtils.js';
+import { students } from './student.js';
+
+console.log("Add:", add(2, 3));        
+console.log("Multiply:", multiply(4, 2)); 
+
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
